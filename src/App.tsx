3cc4d8f61dda116e03628b024a4ffe35eec97a5c
Viniski <799048283mw@gmail.com>
2023-01-11 16:12:23 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header">
+        <h1>Product List App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
